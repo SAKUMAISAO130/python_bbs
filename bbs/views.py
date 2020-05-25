60499c2000fd46fc81ad1d1f601bcf8b75038455
sakuma-tech <colorfullweb@gmail.com>
2020-05-25 22:46:35 +0900

    DBデータを表示させるまで

--- bbs/views.py
+++ bbs/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Article
 
 def index(request):
-    return HttpResponse('Hello world')
+    articles = Article.objects.all()
+    context = {
+        'message':'welcom my bbs',
+        'articles':articles
+    }
+    return render(request, 'bbs/index.html', context)
+
+def detail(request, id):
+    article = get_object_or_404(Article, pk=id)
+    context = {
+        'message':'Detail',
+        'article':article
+    }
+    return render(request, 'bbs/detail.html', context)
